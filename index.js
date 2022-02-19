a7f01d830367ea51a470c6fdd91790fbd8e511bf
habil1hsan <87801232+habil1hsan@users.noreply.github.com>
2022-02-19 11:14:12 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,80 @@
+require('dotenv').config()
+const Twit = require('twit')
+const CronJob = require('cron').CronJob
+const Firebase = require('firebase-admin')
+const serviceAccount = require("E:\\twitter-api-master\\ayang-zee-firebase-adminsdk-4a8v0-667e425f7d.json")
+
+const T = new Twit({
+  consumer_key: 'fUQifvicN8e4WCateHss73yJQ',
+  consumer_secret: 'CXqRMmwlYU4L6odv2DhxhwWAXnQq6jxBt1ZO2frZ8DZ2gcPibS',
+  access_token: '1126042316026703872-V36xr1m39Sy5xJedgzzma1gMFXRtQY',
+  access_token_secret: 'aLO6jampwJh1uTLQsO77OfTkI57pz1gBNTTFA9MaDla6Y',
+  timeout_ms: 60 * 1000
+})
+
+Firebase.initializeApp({
+  credential: Firebase.credential.cert(serviceAccount),
+  databaseURL: process.env.FIREBASE_DB_URL
+})
+const db = Firebase.database()
+const ref = db.ref("/tweetData")
+
+let tweetData = {}
+ref.on("value", function (snapshot) {
+  tweetData = snapshot.val()
+}, function (errorObject) {
+  console.log("The read failed: " + errorObject.code)
+})
+
+//Scheduled tweet
+new CronJob('* * * * *', function () {
+  let failedTweet = []
+
+  const newDate = new Date()
+  const hour = newDate.getHours('en-US', { timezone: 'Asia/Jakarta' })
+  const minute = newDate.getMinutes('en-US', { timezone: 'Asia/Jakarta' })
+  const time = `${hour}:${minute}`
+  console.log('Starting auto tweet....... @' + time)
+
+  for (let i = 0; i < tweetData.scheduledTweet.length; i++) {
+    if (tweetData.scheduledTweet[i].time == time) {
+      T.post('statuses/update', { status: tweetData.scheduledTweet[i].message }, function (err, data, response) {
+        if (err) {
+          console.error('ERROR ====> ' + err.message)
+          console.log(tweetData.scheduledTweet[i])
+          failedTweet.push(tweetData.scheduledTweet[i])
+        } else {
+          console.log('Success tweet: ' + tweetData.scheduledTweet[i].message)
+        }
+      })
+    }
+  }
+
+  setTimeout(() => {
+    failedTweet.length > 0 ? ref.child('failedTweet').set(failedTweet) : ''
+  }, 1000)
+}, null, true, 'Asia/Jakarta').start()
+
+// Failed Tweet
+new CronJob('*/5 * * * * *', function () {
+  if (tweetData.failedTweet) {
+    let failedTweet = tweetData.failedTweet
+    console.log('TWEETING FAILED TWEET!!!!!!')
+    for (let i = 0; i < tweetData.failedTweet; i++) {
+      T.post('statuses/update', { status: tweetData.failedTweet[i].message }, function (err, data, response) {
+        if (err) {
+          console.error('ERROR ====> ' + err.message)
+        } else {
+          console.log('Success tweet: ' + tweetData.failedTweet[i].message)
+          failedTweet.splice(i, 1)
+        }
+      })
+    }
+
+    setTimeout(() => {
+      ref.child('failedTweet').set(failedTweet)
+    }, 1000)
+  } else {
+    console.log('Failed tweet is clear :)')
+  }
+}, null, true, 'Asia/Jakarta').start()
